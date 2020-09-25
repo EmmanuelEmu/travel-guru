6fe24ecc4ddb1424b65f7e22bd563cee749fb7a3
Emmanuel99 <emuhembrom@gmail.com>
2020-09-25 22:10:05 +0600

    final project

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAHCfO9z8NhlNcLw4IVbEtLrGwlpHKCqww",
+    authDomain: "travel-guru-e2193.firebaseapp.com",
+    databaseURL: "https://travel-guru-e2193.firebaseio.com",
+    projectId: "travel-guru-e2193",
+    storageBucket: "travel-guru-e2193.appspot.com",
+    messagingSenderId: "528984396448",
+    appId: "1:528984396448:web:2d8769069c568ea9916fc5"
+  };
+
+export default firebaseConfig;
